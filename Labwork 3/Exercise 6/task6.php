f3c8580c6e8fb14961550950876daa1dcfdaefdf
NoVeTe36 <tungnv.bi12-480@st.usth.edu.vn>
2023-09-24 15:13:56 +0700

    fix task 6 to deploy on xampp

--- Labwork 3/Exercise 6/task6.php
+++ Labwork 3/Exercise 6/task6.php
@@ -1,11 +1,12 @@
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Enter an array: <input type="text" name="array">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Enter an array: <input type="text" name="array">
     <input type="submit">
 </form>
 
 <?php
 function sort_array($array)
 {
+    $temp = 0;
     for ($i = 0; $i < count($array); $i++) {
         for ($j = $i + 1; $j < count($array); $j++) {
             if ($array[$i] > $array[$j]) {
@@ -17,3 +18,19 @@ function sort_array($array)
     }
     return $array;
 }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $array = $_POST['array'];
+    if (empty($array)) {
+        echo "Array is empty";
+    } else {
+        $array = explode(" ", $array);
+        echo "The sorted array is: ";
+        $array = sort_array($array);
+        foreach ($array as $value) {
+            echo $value . " ";
+        }
+    }
+}
+?>
